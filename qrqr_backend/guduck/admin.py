0ce3843d3c884eeb0c3cd7067461a19976bd240c
woosun <p.woosun@gmail.com>
2022-11-26 15:12:17 +0900

    push 테이블 생성

--- qrqr_backend/guduck/admin.py
+++ qrqr_backend/guduck/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.db.models import Q
 
 @admin.register(guduck)
 class GuduckAdmin(admin.ModelAdmin):
--- qrqr_backend/user/models.py
+++ qrqr_backend/user/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from product.utils import rename_imagefile_to_uuid
 
