20d07b9a87dd1c78c0f28d76d1771f35980732bb
wosun <pws@yos.kr>
2022-12-01 13:27:06 +0900

    젠킨스 테스트6

---
+++ qrqr_backend/static/app.js
@@ -0,0 +1,26 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "Api Key goes here",
+    authDomain: "projectid.firebaseapp.com",
+    databaseURL: "https://projectid.firebaseio.com",
+    projectId: "projectid",
+    storageBucket: "projectid.appspot.com",
+    messagingSenderId: ""
+  };
+  firebase.initializeApp(config);
+
+  const messaging = firebase.messaging();
+  messaging.requestPermission().then(function() {
+     //getToken(messaging);
+     return messaging.getToken();
+  }).then(function(token){
+  console.log(token);
+  })
+.catch(function(err) {
+  console.log('Permission denied', err);
+});
+
+
+messaging.onMessage(function(payload){
+console.log('onMessage: ',payload);
+});
