d790ec869737d92f33884b65cc16b00fd933e3bd
gedflow <upmoo1327@gmail.com>
2022-12-01 14:22:14 +0900

    change test

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 cd /app/qrqr_backend
 pip install django pymysql gunicorn Pillow
-mv /app/secret.json /app/qrqr_backend/secret.json
+cp /app/secret.json /app/qrqr_backend/secret.json
 python manage.py migrate
 gunicorn --bind 0.0.0.0:8000 config.wsgi:application
 python manage.py runserver
