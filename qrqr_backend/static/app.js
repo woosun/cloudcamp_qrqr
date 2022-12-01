b46b1eb778d05219baa8a5c43f2f57a34b5aae54
wosun <pws@yos.kr>
2022-12-01 15:39:04 +0900

    docker build

---
+++ qrqr_backend/static/app.js
@@ -0,0 +1,15 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaqn2VoaA4-0nAQ55DX3FyF8hY92b3Zh4",
+    authDomain: "qrqr-c2114.firebaseapp.com",
+    projectId: "qrqr-c2114",
+    storageBucket: "qrqr-c2114.appspot.com",
+    messagingSenderId: "748054356351",
+    appId: "1:748054356351:web:56b9b76bfc555b6c033a4a",
+    measurementId: "G-687SWR2XS2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
