6f15329cd0a3c7c51d9c0e35660a9d6e1dc275bb
woosun <p.woosun@gmail.com>
2022-12-03 11:59:42 +0900

    게시판 구독  페이징 처리

--- qrqr_backend/guduck/views.py
+++ qrqr_backend/guduck/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from guduck.models import guduck
 from product.models import Product
-
+from django.core.paginator import Paginator
 # 리스트가 불러옴
 @login_required(login_url='login') #나의 구독리스트 이므로 로그인여부 체크
 def list(request):
-    lists = guduck.objects.filter(uid_id = request.user.id,read_yn=1).order_by('-id')
+
+    g_lists = guduck.objects.filter(uid_id = request.user.id,read_yn=1).order_by('-id')
+    # board_list 페이징 처리
+    page = request.GET.get('page', '1')  # GET 방식으로 정보를 받아오는 데이터
+    paginator = Paginator(g_lists, '3')  # Paginator(분할될 객체, 페이지 당 담길 객체수)
+    lists = paginator.page(page)  # 페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+
     return render(request, 'guduck/index.html', {'lists': lists, 'request': request})
 
 @login_required(login_url='login') #나의 구독리스트 이므로 로그인여부 체크
 def unlike(request,pid):
+    spage = request.GET.get('page','')
     # 어떤 게시물에, 어떤 사람이 like를 했는 지
     post = Product.objects.get(id=pid)  # 게시물 번호 몇번인지 정보 가져옴
     user = request.user
@@ -18,7 +25,10 @@ def unlike(request,pid):
         print("구독 삭제")
         guduck_fc(request.user.id, pid, "remove")
         post.like.remove(user)
-    return redirect('/myguduck/')
+    if spage :
+      return redirect('/myguduck/?page='+spage)
+    else :
+      return redirect('/myguduck/')
 
 def guduck_fc(uid, pid, type):
     # 구독여부확인하여
