a4b6dec808a926e2ee48ceedc81ff0682425f5c9
woosun <p.woosun@gmail.com>
2022-12-08 22:28:55 +0900

    s3로 이전작업

--- qrqr_backend/config/settings.py
+++ qrqr_backend/config/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pymysql
 import os
 import json
-import firebase_admin
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'product',
+    'storages',
     'guduck',
     'user',
     'push',
@@ -128,16 +128,27 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 
+# AWS Setting
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+AWS_STORAGE_BUCKET_NAME = 'qrqr-static'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % secrets['AWS_REGION']
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# s3 static settings
+AWS_LOCATION = 'static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+# Static Setting
+STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+
+#Media Setting
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
 
 
 # Default primary key field type
--- qrqr_backend/user/forms.py
+++ qrqr_backend/user/forms.py
@@ -1,20 +1,6 @@
 from django import forms
-# from user.models import Profile
 from user.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields = (
-#             'user_img',
-#             'bio',
-#             'location',
-#             'birth_date'
-#         )
-
-
-
 class cSignupForm(UserCreationForm):
 
     email=forms.EmailField(label="email")
